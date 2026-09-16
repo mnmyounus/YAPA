117006d5f24280d52ffe5c1152beaa57c9d1d282
MNM YOUNUS <mnmyounus@gmail.com>
2026-09-16 12:04:18 +0530

    Add lifecycle-runtime-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
 
     // Android TV / Leanback focus support
     implementation("androidx.tv:tv-foundation:1.0.0-alpha11")
